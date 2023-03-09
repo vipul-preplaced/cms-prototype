041f77475771c1ab9147d41c128b058a8d3c30ef
vipul-preplaced <vipul.bansal@preplaced.in>
2023-03-09 22:59:30 +0530

    Schema for blog and author added

--- src/api/author/services/author.js
+++ src/api/author/services/author.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * restaurant service
+ * author service
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::restaurant.restaurant');
+module.exports = createCoreService('api::author.author');
